2c47182732c3dc60ee55a525f67c731cf033a924
Seongjun Lee <sl.seongjunlee@gmail.com>
2025-06-22 14:27:47 +0900

    shadcn 버튼 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
+import '@/App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  return <>App</>
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
 export default App
