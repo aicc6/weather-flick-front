ad688933a05b92847b1dc40c2a795a204b1cd2ea
Hong <syun4224@gmail.com>
2025-07-03 18:01:03 +0900

    맞춤 일정 복원

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import { SignUpPage } from './pages/sign-up'
 import { ReviewsPage } from './pages/reviews'
 import { ReviewWritePage } from './pages/reviews/write'
 import RecommendPage from './pages/recommend'
+import RecommendRegionPage from './pages/recommend/region'
+import RecommendPeriodPage from './pages/recommend/period'
+import RecommendWhoPage from './pages/recommend/who'
+import RecommendStylePage from './pages/recommend/style'
+import RecommendSchedulePage from './pages/recommend/schedule'
+import RecommendResultPage from './pages/recommend/result'
 import { VerifyEmailPage } from './pages/auth/verify-email'
 import { GoogleCallbackPage } from './pages/auth/GoogleCallback'
 import { ProfilePage } from './pages/profile'
@@ -27,7 +33,31 @@ function App() {
           <Route path="/recommend" element={<TravelCoursePage />} />
           <Route path="/reviews" element={<ReviewsPage />} />
           <Route path="/reviews/write" element={<ReviewWritePage />} />
-          <Route path="/customized-schedule/*" element={<RecommendPage />} />
+          <Route path="/customized-schedule" element={<RecommendPage />} />
+          <Route
+            path="/customized-schedule/region"
+            element={<RecommendRegionPage />}
+          />
+          <Route
+            path="/customized-schedule/period"
+            element={<RecommendPeriodPage />}
+          />
+          <Route
+            path="/customized-schedule/who"
+            element={<RecommendWhoPage />}
+          />
+          <Route
+            path="/customized-schedule/style"
+            element={<RecommendStylePage />}
+          />
+          <Route
+            path="/customized-schedule/schedule"
+            element={<RecommendSchedulePage />}
+          />
+          <Route
+            path="/customized-schedule/result"
+            element={<RecommendResultPage />}
+          />
           <Route path="/recommend/:id" element={<TravelCourseDetailPage />} />
           <Route path="/planner" element={<PlannerPage />} />
           <Route path="/auth/verify-email" element={<VerifyEmailPage />} />
