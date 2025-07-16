4ceeeb93c230af53c90f7ae6fb37bbe4f524920e
Seongjun Lee <sl.seongjunlee@gmail.com>
2025-07-16 11:56:01 +0900

    fix: 비밀번호 변경 페이지 404 에러 수정

--- src/App.jsx
+++ src/App.jsx
@@ -148,7 +148,7 @@ function AppContent() {
           />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/profile/edit" element={<ProfileEditPage />} />
-          <Route path="/change-password" element={<ChangePasswordPage />} />
+          <Route path="/profile/change-password" element={<ChangePasswordPage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/help" element={<HelpPage />} />
           <Route path="/contact" element={<ContactPage />} />
